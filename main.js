06b528c49f36addab015f800a2daaca2dec51009
Diyorbek <diyorbekolimov2000@gmail.com>
2022-08-18 12:55:32 +0500

    fixed bug in products

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
+let dropdowns = document.getElementsByClassName("hidden_dropdown");
 let count = document.getElementsByClassName("count");
 let products = document.getElementsByClassName("table_bottom");
 let cart_empty = document.getElementsByClassName("cart_empty")[0];
+let cart = document.getElementsByClassName("cart")[0];
 let table_top = document.getElementsByClassName("table_top")[0];
 let btn_div = document.getElementsByClassName("btn_div")[0];
-let counter = 0;
+let arrows = document.querySelectorAll(".main_catalog .dropdown img");
+
+let saver = false,
+  counter = 0;
 
 function decrement(index) {
   if (count[index].innerHTML > 1)
@@ -22,4 +27,28 @@ function removeProduct(index) {
     btn_div.style.display = "none";
   }
   products[index].style.display = "none";
+}
+
+function showCart() {
+  cart.style.display = "block";
+  document.body.style.overflow = "hidden";
+  document.getElementsByClassName("bg")[0].style.display = "block";
+}
+
+function hideCart() {
+  cart.style.display = "none";
+  document.body.style.overflow = "auto";
+  document.getElementsByClassName("bg")[0].style.display = "none";
+}
+
+function toggleDropdown(index) {
+  if (saver) {
+    dropdowns[index].style.display = "none";
+    arrows[index].style.transform = "rotate(0deg)";
+    saver = false;
+  } else {
+    saver = true;
+    dropdowns[index].style.display = "block";
+    arrows[index].style.transform = "rotate(90deg)";
+  }
 }
